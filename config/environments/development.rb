cec03e124e78aea67294853bb6dc555879fe0566
OmkarKakade <saiprasaddevare12@gmail.com>
2017-12-05 14:38:41 +0530

    Account-Activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,11 +28,8 @@
 
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'nameless-savannah-89652.herokuapp.com'   # Don't use this literally; use your local dev host instead
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-
+  config.action_mailer.raise_delivery_errors = false
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -55,4 +52,10 @@
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:8000' # Don't use this literally; use your local dev host instead
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
