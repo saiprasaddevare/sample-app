8feaef7d02052f053b30b36bafaf7785c36c7050
OmkarKakade <omiekakade007@gmail.com>
2017-12-05 07:54:35 +0530

    Add password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,15 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  def index
+    # @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
+
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    # redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -19,13 +21,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     UserMailer.account_activation(@user).deliver_now
+  #     flash[:info] = "Please check your email to activate your account."
+  #     redirect_to root_url
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
   def edit
     @user = User.find(params[:id])
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nameless-savannah-89652.herokuapp.com'
+  host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -77,7 +77,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
